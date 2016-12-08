465f844f1b23a5cf8ec8a1036a74190b5d27813e
Jeremy Pitt <jeremypitt@Jeremys-MacBook-Pro.local>
2016-12-08 13:45:57 -0600

    Fixed sidenav

--- templates/js/init.js
+++ templates/js/init.js
@@ -1,7 +1,14 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    // $('.button-collapse').sideNav();
+
+    $('.button-collapse').sideNav({
+      menuWidth: 300,
+      edge: 'left',
+      closeOnClick: true,
+      draggable: true
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
